c16e52b98850b20cf5398e6ece000832c8ef385d
htbkoo <11581279+htbkoo@users.noreply.github.com>
2025-03-04 08:55:31 +0100

    feat(script): Add logging

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,16 @@
+console.debug("=>(service-worker.js:2) scripte loaded");
+
 /**
  * Reference: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/onCreated
  */
 chrome.tabs.onCreated.addListener(async (tab) => {
+  console.debug("=>(service-worker.js:2) onCreated, tab.id: ", tab.id);
+
   try {
     // Reference: https://www.reddit.com/r/learnjavascript/comments/rwo19m/comment/hrcwqhc/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
     await chrome.tabs.update(tab.id, { muted: true });
+    console.debug("=>(service-worker.js:2) muted tab");
   } catch (error) {
-    console.error(error);
+    console.error("=>(service-worker.js:2) failed", error);
   }
 });
