afc621ebea6563d90a90ad1c2c0f0d3ad691f908
htbkoo <11581279+htbkoo@users.noreply.github.com>
2025-03-07 17:58:47 +0100

    feat(build): Add `bumpVersion.js`

---
+++ bin/bumpVersion.js
@@ -0,0 +1,92 @@
+/**
+ * Get new version string based on the current version
+ * @param version {string}
+ * @returns {string}
+ */
+const getNewVersion = (version) => {
+  const versionParts = version.split(".");
+  if (versionParts.length !== 3) {
+    throw new Error(`Version ${version} is not in the format of x.y.z`);
+  }
+
+  const [major, minor, patch] = versionParts;
+
+  const minorVersion = parseInt(minor);
+  if (!Number.isInteger(minorVersion)) {
+    throw new Error(
+      `Minor version ${minor} of version ${version} is not an integer`,
+    );
+  }
+  return `${major}.${minorVersion + 1}.0`;
+
+  /*
+  // TODO: accept a parameter to specify which part of the version to bump, or an exact version to bump to
+    if (versionParts[0] === "0") {
+      const minorVersion = parseInt(minor);
+      if (!Number.isInteger(minorVersion)) {
+        throw new Error(
+          `Minor version ${minor} of version ${version} is not an integer`,
+        );
+      }
+      return `${major}.${minorVersion + 1}.0`;
+    }
+
+    const majorVersion = parseInt(major);
+    if (!Number.isInteger(majorVersion)) {
+      throw new Error(
+        `Minor version ${majoror} of version ${version} is not an integer`,
+      );
+    }
+    return `${major}.0.0`;
+    */
+};
+
+(async () => {
+  console.log("Bumping version of `edge-extension-mute-tab-by-default`");
+
+  const path = require("path");
+  const fs = require("node:fs/promises");
+
+  const { cpWithLogging, isFileExist } = require("./utils");
+  const {
+    BUILD_FOLDER_PATH,
+    PROJECT_ROOT_PATH,
+    SRC_FOLDER_PATH,
+    MANIFEST_PATH,
+    PACKAGE_JSON_PATH,
+  } = require("./constants");
+
+  const manifestString = await fs.readFile(MANIFEST_PATH, { encoding: "utf8" });
+  const manifestJson = JSON.parse(manifestString);
+  const packageString = await fs.readFile(PACKAGE_JSON_PATH, {
+    encoding: "utf8",
+  });
+  const packageJson = JSON.parse(packageString);
+
+  if (!("version" in manifestJson)) {
+    throw new Error("manifest.json does not have version field");
+  }
+  if (!("version" in packageJson)) {
+    throw new Error("package.json does not have a version field");
+  }
+  if (manifestJson.version !== packageJson.version) {
+    throw new Error(
+      `Version in manifest.json (${manifestJson.version}) is different from version in package.json (${packageJson.version})`,
+    );
+  }
+
+  const version = manifestJson.version;
+  const newVersion = getNewVersion(version);
+
+  // Modify the version in-place to reserve the order of the keys
+  manifestJson.version = newVersion;
+
+
+
+  packageJson.version = newVersion;
+
+  const newManifestJson = { ...manifestJson, version: newVersion };
+  const newPackageJson = { ...packageJson, version: newVersion };
+
+  console.log("Bum version complete");
+})();
--- bin/constants.js
+++ bin/constants.js
@@ -3,9 +3,21 @@ const path = require("path");
 const PROJECT_ROOT_PATH = path.normalize(`${__dirname}/..`);
 const SRC_FOLDER_PATH = path.normalize(`${PROJECT_ROOT_PATH}/src`);
 const BUILD_FOLDER_PATH = path.normalize(`${PROJECT_ROOT_PATH}/build`);
+const MANIFEST_FILENAME = `manifest.json`;
+const MANIFEST_PATH = path.normalize(
+  `${PROJECT_ROOT_PATH}/${MANIFEST_FILENAME}`,
+);
+const PACKAGE_JSON_FILENAME = `package.json`;
+const PACKAGE_JSON_PATH = path.normalize(
+  `${PROJECT_ROOT_PATH}/${PACKAGE_JSON_FILENAME}`,
+);
 
 module.exports = {
   PROJECT_ROOT_PATH,
   SRC_FOLDER_PATH,
   BUILD_FOLDER_PATH,
+  MANIFEST_FILENAME,
+  MANIFEST_PATH,
+  PACKAGE_JSON_FILENAME,
+  PACKAGE_JSON_PATH,
 };
