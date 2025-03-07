b9fffeb81ff0255f0d2328b08befc214545856e5
htbkoo <11581279+htbkoo@users.noreply.github.com>
2025-03-07 15:37:12 +0100

    feat(build): Update `bin/build.js`

--- bin/build.js
+++ bin/build.js
@@ -1,8 +1,23 @@
-(() => {
+/**
+ *
+ * @param path {string}
+ * @returns {Promise<boolean>}
+ */
+const isFileExist = (path) => {
+  const fs = require("node:fs");
+
+  return new Promise((resolve) => {
+    fs.access(path, fs.constants.F_OK, (err) => {
+      resolve(!err);
+    });
+  });
+};
+
+(async () => {
   console.log("Building `edge-extension-mute-tab-by-default`");
 
   const path = require("path");
-  const fs = require("fs");
+  const fs = require("node:fs/promises");
 
   const PROJECT_ROOT_PATH = path.normalize(`${__dirname}/..`);
   const SRC_FOLDER_PATH = path.normalize(`${PROJECT_ROOT_PATH}/src`);
@@ -11,19 +26,28 @@
     `${SRC_FOLDER_PATH}/service-worker/${SERVICE_WORKER_SCRIPT_FILENAME}`,
   );
   const BUILD_FOLDER_PATH = path.normalize(
-    `${PROJECT_ROOT_PATH}/BUILD_FOLDER_PATH`,
+    `${PROJECT_ROOT_PATH}/build`,
   );
   const BUILD_SERVICE_WORKER_SCRIPT_PATH = path.normalize(
     `${BUILD_FOLDER_PATH}/${SERVICE_WORKER_SCRIPT_FILENAME}`,
   );
 
+  if (await isFileExist(BUILD_FOLDER_PATH)) {
+    console.log("Deleting build/ folder");
+    await fs.rmdir(BUILD_FOLDER_PATH, { recursive: true });
+    console.log("Deleted build/ folder");
+  }
+
+  console.log("Creating build/ folder");
+  await fs.mkdir(BUILD_FOLDER_PATH);
+  console.log("Created build/ folder");
+
   console.log("Copying service-worker.js");
-  fs.copyFile(SERVICE_WORKER_SCRIPT_PATH, BUILD_SERVICE_WORKER_SCRIPT_PATH, (err) => {
-    if (err) {
-      throw err;
-    }
-    console.log("Copied service-worker.js");
-  });
+  await fs.copyFile(
+    SERVICE_WORKER_SCRIPT_PATH,
+    BUILD_SERVICE_WORKER_SCRIPT_PATH,
+  );
+  console.log("Copied service-worker.js");
 
   console.log("Build complete");
 })();
