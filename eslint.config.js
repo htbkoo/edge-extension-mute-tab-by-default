ad23cd072b7d31255255616961cb578abe45c61c
htbkoo <11581279+htbkoo@users.noreply.github.com>
2025-03-08 12:42:23 +0100

    chore(eslint): Update `eslint.config.js` to fix issues

--- eslint.config.js
+++ eslint.config.js
@@ -9,17 +9,27 @@ export default tseslint.config(
   // reference: https://typescript-eslint.io/getting-started#step-2-configuration
   eslint.configs.recommended,
   tseslint.configs.recommended,
+
+  // reference: https://eslint.org/docs/latest/use/configure/migration-guide#ignoring-files
+  {
+    // Note: there should be no other properties in this object
+    ignores: ["dist", "build", "publish", "node_modules"],
+  },
   // reference: https://typescript-eslint.io/troubleshooting/faqs/general#how-do-i-turn-on-a-typescript-eslint-rule
   {
-    env: {
-      // reference: https://stackoverflow.com/a/50513752/10734272
-      webextensions: true,
-    },
-    ignores: ["dist", "build", "publish"],
-    files: ["**/*.{js,jsx,ts,tsx}"],
+    files: ["**/*.{mjs,js,jsx,ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        /**
+         * For fixing `chrome` undefined error in eslint
+         * References:
+         * 1. https://stackoverflow.com/a/50513752/10734272
+         * 2. https://eslint.org/docs/latest/use/configure/migration-guide#configuring-language-options
+         */
+        ...globals.webextensions,
+      },
       parserOptions: {
         ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
