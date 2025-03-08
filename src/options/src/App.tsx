bc37d42bf6c702c78ff1024eab69bb69a411256d
htbkoo <11581279+htbkoo@users.noreply.github.com>
2025-03-08 13:49:18 +0100

    feat(OptionsPage): Adapt `react-bootstrap` style

--- src/options/src/App.tsx
+++ src/options/src/App.tsx
@@ -1,21 +1,10 @@
 // Reference: https://github.com/aelbore/esbuild-jest/issues/61#issuecomment-990032621
 import * as React from "react";
-import { useState, useActionState, useCallback } from "react";
+import { useActionState, useCallback } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 import "./App.css";
-
-const useSyncStorage = (key, defaultValue) => {
-  const [value, setValue] = useState(() => {
-    const storedValue = localStorage.getItem(key);
-    return storedValue !== null ? JSON.parse(storedValue) : defaultValue;
-  });
-
-  const setStoredValue = (newValue) => {
-    setValue(newValue);
-    localStorage.setItem(key, JSON.stringify(newValue));
-  };
-
-  return [value, setStoredValue];
-};
 
 interface FormStateType {
   isWhitelistMode: boolean;
@@ -25,8 +14,15 @@ const DEFAULT_FORM_STATE = {
   isWhitelistMode: true,
 } as const satisfies FormStateType;
 
+const Mode = () => {
+  return null;
+};
+
 export const App = () => {
-  const [{isWhitelistMode}, formAction, isPending] = useActionState<FormStateType, P>(
+  const [{ isWhitelistMode }, formAction, isPending] = useActionState<
+    FormStateType,
+    {}
+  >(
     useCallback((prevState, payload) => {
       return {
         isWhitelistMode: true,
@@ -34,37 +30,58 @@ export const App = () => {
     }, []),
     DEFAULT_FORM_STATE,
   );
+
   return (
     <>
       <h1>Mute Tab By Default</h1>
-
-      <form action={formAction}>
+      <Form action={formAction}>
         <fieldset>
-          <legend>Please select your preferred contact method:</legend>
-          <div>
-            <input
+          <legend>Mode:</legend>
+
+          <Form.Group className="my-6">
+            <Form.Check
               type="radio"
               id="whitelist"
               name="whitelistMode"
               value="whitelist"
               checked={isWhitelistMode}
+              label="Whitelist mode"
+              className="mb-3"
             />
-            <label htmlFor="whitelist">Always mute expect these domains (whitelist):</label>
 
-            <input
+            <Form.Label htmlFor="whitelistTextInput">
+              Always mute expect these domains:
+            </Form.Label>
+            <Form.Control
+              id="whitelistTextInput"
+              placeholder="Domain (e.g. www.wikipedia.org)"
+            />
+          </Form.Group>
+
+          <Form.Group className="my-6">
+            <Form.Check
               type="radio"
               id="blacklist"
               name="whitelistMode"
               value="blacklist"
+              checked={!isWhitelistMode}
+              label="Always mute expect these domains (whitelist):"
+              className="mb-3"
             />
-            <label htmlFor="blacklist">Only mute these domains (blacklist):</label>
+            <Form.Label htmlFor="blacklistTextInput">
+              Always mute expect these domains:
+            </Form.Label>
+            <Form.Control
+              id="blacklistTextInput"
+              placeholder="Domain (e.g. www.wikipedia.org)"
+            />
+          </Form.Group>
 
-          </div>
-          <div>
-            <button type="submit">Save</button>
-          </div>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
         </fieldset>
-      </form>
+      </Form>
     </>
   );
 };
