b8cc590a1fb4c13c6a3d2fe9a37dcad8fb3d3d66
htbkoo <11581279+htbkoo@users.noreply.github.com>
2025-03-08 16:06:17 +0100

    feat(ServiceWorker): Implement configs-driven behaviour

--- src/service-worker/service-worker.ts
+++ src/service-worker/service-worker.ts
@@ -1,4 +1,69 @@
+import { ChromeLocalStorage } from "../storage";
+import type { ConfigsType } from "../types";
+import { STORAGE_KEY_CONFIGS } from "../constants";
+
 console.debug("=>(service-worker.js) script loaded");
+
+// TODO: improve this
+let configsCache: null | ConfigsType = null;
+
+ChromeLocalStorage.listenOnChanged((storage) => {
+  if (storage[STORAGE_KEY_CONFIGS]) {
+    configsCache = storage[STORAGE_KEY_CONFIGS].newValue;
+  }
+});
+
+const initConfigs = async () => {
+  if (configsCache) {
+    // lazy load to avoid unnecessary reads from storage for every tab creation
+    console.debug("=>(service-worker.js) configs already loaded, not loading again");
+    return;
+  }
+
+  console.debug("=>(service-worker.js) loading configs");
+  // TODO: implement exponential backoff
+  const configs = await ChromeLocalStorage.getConfigs();
+  if (configs) {
+    console.debug("=>(service-worker.js) configs loaded");
+    configsCache = configs;
+  }
+};
+
+/**
+ * This function returns if the tab should be muted
+ */
+const shouldMuteTab = async (tab: chrome.tabs.Tab): Promise<boolean> => {
+  if (!configsCache) {
+    // unable to determine if the tab should be muted, so don't mute to be safe
+    console.warn("=>(service-worker.js) unable to load config, not muting tab");
+    return false;
+  }
+
+  const { isWhitelistMode, whitelist, blacklist } = configsCache;
+
+  const tabUrl = tab.pendingUrl;
+  if (!tabUrl) {
+    // unable to determine the url of the tab, so falling back to determine if the tab should be muted based on the mode
+    console.warn(
+      `=>(service-worker.js) unable to determine tab url, using the mode preferred to determine if the tab should be muted: isWhitelistMode = ${isWhitelistMode}`,
+    );
+    return isWhitelistMode;
+  }
+
+  const url = new URL(tabUrl);
+  if (isWhitelistMode) {
+    const whitelistHostnames = new Set(whitelist.split("\n"));
+
+    console.log("debugdebug=>(service-worker.ts:58) ", whitelistHostnames, url.hostname);
+
+    return !whitelistHostnames.has(url.hostname);
+  } else {
+    const blacklistHostnames = new Set(blacklist.split("\n"));
+
+    console.log("debugdebug=>(service-worker.ts:64) ", blacklistHostnames, url.hostname);
+    return blacklistHostnames.has(url.hostname);
+  }
+};
 
 /**
  * This function mutes the tab with the given tabId.
@@ -20,8 +85,13 @@ chrome.tabs.onCreated.addListener(async (tab) => {
   console.debug("=>(service-worker.js) onCreated, tab.id: ", tab.id);
 
   try {
-    await muteTab(tab.id);
-    console.debug("=>(service-worker.js) muted tab");
+    await initConfigs();
+    if (await shouldMuteTab(tab)) {
+      await muteTab(tab.id);
+      console.debug("=>(service-worker.js:2) muted tab");
+    } else {
+      console.debug("=>(service-worker.js:2) should not mute tab");
+    }
   } catch (error) {
     console.error("=>(service-worker.js) failed", error);
   }
